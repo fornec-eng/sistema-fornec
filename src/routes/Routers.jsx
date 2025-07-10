5c642df4ddd4eb0e9c34c363473c0772c93c2fe1
Roberta Avelino <70297387+Roberta-Oliveira@users.noreply.github.com>
2025-07-10 14:54:41 -0300

    Update Routers.jsx

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,60 +1,39 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import PrivateRoute from '../routes/PrivateRoute'
+import { Route, Routes } from "react-router-dom"
+import PrivateRoute from "../routes/PrivateRoute"
 
-
-//pages
-
-import Login from "../pages/login";
-import Home from "../pages/home";
-import Cadastro from "../pages/cadastro";
-import Cards from "../components/Cards";
-import Financeiro from "../pages/financeiro";
-import Inventario from "../pages/inventario";
-import Usuarios from "../pages/usuarios";
-import Pagamento_semanal from "../pages/pagamento_semanal";
-import Obras_ativas from "../pages/obras_ativas";
-import GestaoPagamentos from "../pages/GestaoPagamentos";
-import AdicionarPagamentos from "../pages/adicionar-pagamentos";
+// Pages
+import Login from "../pages/login"
+import Home from "../pages/home"
+import Cadastro from "../pages/cadastro"
+import Financeiro from "../pages/financeiro"
+import Inventario from "../pages/inventario"
+import Usuarios from "../pages/usuarios"
+import Pagamento_semanal from "../pages/pagamento_semanal"
+import ObrasAtivas from "../pages/obras_ativas"
+import AdicionarPagamentos from "../pages/adicionar-pagamentos"
+import ObraDashboard from "../pages/obra-dashboard"
 
 const Routers = () => {
   return (
-    <>
-        <Routes>
-            <Route path="/" element={<PrivateRoute />} >
-                <Route path="/" element={<Home />} />
-            </Route>
-            <Route path="/home" element={<PrivateRoute />} >
-                <Route path="/home" element={<Home />} />
-            </Route> 
-            <Route path="/obras_ativas" element={<PrivateRoute />} >
-                <Route path="/obras_ativas" element={<Obras_ativas />} />
-            </Route>
-            <Route path="/financeiro" element={<PrivateRoute />} >
-                <Route path="/financeiro" element={<Financeiro />} />
-            </Route>
-            <Route path="/usuarios" element={<PrivateRoute />} >
-                <Route path="/usuarios" element={<Usuarios />} />
-            </Route>  
-            <Route path="/pagamento_semanal" element={<PrivateRoute />} >
-                <Route path="/pagamento_semanal" element={<Pagamento_semanal />} />
-            </Route>
-            <Route path="/adicionar-pagamentos" element={<PrivateRoute />} >
-                <Route path="/adicionar-pagamentos" element={<AdicionarPagamentos />} />
-            </Route>          
+    <Routes>
+      {/* Rotas Públicas */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
 
-            <Route path="/inventario" element={<Inventario />} />   
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/gestao-pagamentos" element={<GestaoPagamentos />} />
+      {/* Rotas Privadas */}
+      <Route path="/" element={<PrivateRoute />}>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="obras_ativas" element={<ObrasAtivas />} />
+        <Route path="obras/:id" element={<ObraDashboard />} /> {/* Rota atualizada */}
+        <Route path="financeiro" element={<Financeiro />} />
+        <Route path="usuarios" element={<Usuarios />} />
+        <Route path="pagamento_semanal" element={<Pagamento_semanal />} />
+        <Route path="adicionar-pagamentos" element={<AdicionarPagamentos />} />
+        <Route path="inventario" element={<Inventario />} />
+      </Route>
+    </Routes>
+  )
+}
 
-            {/* Nova rota para o dashboard, recebendo o ID da obra */}
-            <Route path="/dashboard/:id" element={<PrivateRoute />}>
-                <Route path="/dashboard/:id" element={<Cards />} />
-            </Route>
-        </Routes>
-    </>
-  );
-};
-
-export default Routers;
+export default Routers
